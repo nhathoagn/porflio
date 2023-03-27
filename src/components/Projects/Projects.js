7acd7ae658e0e466b19d99767990af2ff9cfca5e
nhathoagn <nguyennhathoang35@gmail.com>
2023-03-27 16:52:48 +0700

    profile

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/emotion.png";
+import editor from "../../Assets/Projects/codeEditor.png";
+import chatify from "../../Assets/Projects/chatify.png";
+import suicide from "../../Assets/Projects/suicide.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="Social Media"
+              description="Build a social networking site with functions of posting, commenting,
+              following, messaging, videocall, audio call
+              "
+              ghLink="https://github.com/soumyajit4419/Chatify"
+              demoLink="https://blogs.soumya-jit.tech/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="Web Chat Realtime"
+              description="Use ReactJS, SocketIO, WebRTC to solve real-time messaging, videocall
+              problems"
+              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
+              demoLink="https://blogs.soumya-jit.tech/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="Youtube Clone"
+              description="Build a website with the same functions as youtube.
+              "
+              ghLink="https://github.com/soumyajit4419/Editor.io"
+              demoLink="https://blogs.soumya-jit.tech/"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
